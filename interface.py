4593359d89da0d6e2cec4ec242e94e7f8a60448e
hugowenner <hugowenner5@gmail.com>
2025-03-28 08:17:54 -0300

    Adicionando lista de ações para análise

--- interface.py
+++ interface.py
@@ -7,11 +7,18 @@ import pandas as pd
 # Configuração da página
 st.set_page_config(page_title="Analisador de Ações", layout="wide")
 
-# Lista de ações disponíveis
+# Lista de ações disponíveis (mais de 50 ações)
 lista_acoes = [
-    "PETR4.SA", "VALE3.SA", "ITUB4.SA", "BBDC4.SA", "BBAS3.SA",
-    "WEGE3.SA", "ABEV3.SA", "MGLU3.SA", "RENT3.SA", "GGBR4.SA",
-    "AAPL", "GOOG", "MSFT", "TSLA", "AMZN"
+    'PETR4.SA', 'ITUB4.SA', 'BBDC4.SA', 'VALE3.SA', 'BBAS3.SA', 'ABEV3.SA', 'MGLU3.SA',
+    'B3SA3.SA', 'WEGE3.SA', 'RADL3.SA', 'RENT3.SA', 'HAPV3.SA', 'GGBR4.SA', 'CSNA3.SA',
+    'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA', 'META', 'NVDA', 'NFLX', 'BRK-B', 'V',
+    'JNJ', 'PG', 'DIS', 'XOM', 'CVX', 'KO', 'PEP', 'MCD', 'INTC', 'CSCO',
+    'SPY', 'QQQ', 'DIA', 'IVV', 'VTI', 'VOO', 'ARKK', 'XLK', 'XLF', 'XLE',
+    'BMW.DE', 'DAI.DE', 'SAP.DE', 'HSBA.L', 'BABA', 'TCEHY', 'TSM', 'NIO', 'JD', 'SONY',
+    'BTC-USD', 'ETH-USD', 'BNB-USD', 'XRP-USD', 'ADA-USD', 'SOL-USD', 'DOT-USD', 'DOGE-USD',
+    'MATIC-USD', 'LTC-USD', 'SHIB-USD', 'AVAX-USD', 'LINK-USD', 'XLM-USD', 'UNI-USD', 'ALGO-USD',
+    'TRX-USD', 'NEO-USD', 'XTZ-USD', 'MKR-USD', 'AAVE-USD', 'YFI-USD', 'COMP-USD', 'CRV-USD',
+    'SUSHI-USD', 'KSM-USD', 'FIL-USD', 'RUNE-USD', 'EGLD-USD', 'KAVA-USD'
 ]
 
 # Título do app
